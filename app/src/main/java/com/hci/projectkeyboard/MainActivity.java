a365e3eed9d3b377e52800526598085a48a7a69b
Kushal Shah <skushal094@gmail.com>
2021-04-12 17:12:48 +0530

    added radio buttons in activity to select the keyboard type

--- app/src/main/java/com/hci/projectkeyboard/MainActivity.java
+++ app/src/main/java/com/hci/projectkeyboard/MainActivity.java
@@ -2,13 +2,41 @@ package com.hci.projectkeyboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    Button button;
+
+    @Override
+    public void onBackPressed() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerButton();
+    }
+
+    private void addListenerButton() {
+        radioGroup = findViewById(R.id.radioGroup);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            int selectedID = radioGroup.getCheckedRadioButtonId();
+            radioButton = findViewById(selectedID);
+            Intent intent = new Intent(MainActivity.this, TrialActivity.class);
+            intent.putExtra("keyboard_type", radioButton.getText().toString());
+            startActivity(intent);
+        });
     }
 }
